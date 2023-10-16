d58b13526c79c919998d842dc238a67927535a96
SLAVACOM <slavacom121@gmail.com>
2023-10-16 18:50:29 +0300

    finish 1

--- 1.py
+++ 1.py
@@ -1,17 +1,19 @@
 print('           (        1        )')
 print('6. y(x)=SUM(----------------,),  |x|>=1 ')
 print('           ( (2n+1)*x^(2n+1) )')
-x = num = float(input("Введите x: "))
+x = float(input("Введите x: "))
+
 while (abs(x) <= 1):
-    print('Нарушено условие! Введите число x заново')
-    x = num = float(input("Введите x: "))
-summ = 1/x
+    print('Нарушено условие! Введите x заново')
+    x = float(input("Введите x: "))
+summ = 1 / x
+
 k = 1
+element_posleovatelnosti = 1 / x
 while True:
-    k += 2
-    num = x * x * num
-    posled = 1/(k*num)
-    if posled < 0.000_001:
+    element_posleovatelnosti = element_posleovatelnosti * k / ((k + 2) * x * x)
+    summ += element_posleovatelnosti
+    if element_posleovatelnosti < 0.000_001:
         break
-    summ+=posled
-print(f"Сумма: {summ}")
+    k += 2
+print(f"Сумма: {summ} ")
